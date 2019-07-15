b5c6b08917cd0ce9295c8ebf216c1e409af0db8f
Bertho <bertho@pawoon.com>
2019-07-15 12:36:13 +0700

    Generate random password

--- src/pages/PasswordGenerator.js
+++ src/pages/PasswordGenerator.js
@@ -1,6 +1,32 @@
 import React, { Component } from "react";
+import PassGen from "generate-password";
  
 class PasswordGenerator extends Component {
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			txtpassword: "",
+			defaulLength: 12
+		}
+		
+	}
+
+	generatePassword = () => {
+		var password = PassGen.generate({
+			length: this.state.defaulLength,
+			numbers: true,
+			uppercase: true,
+			symbols: true,
+			strict: true,
+			excludeSimilarCharacters: true
+		});
+
+		this.setState({
+			txtpassword : password
+		});
+	}
+
 	render() {
 		return (
 			<div className="row">
@@ -8,7 +34,14 @@ class PasswordGenerator extends Component {
 					<div className="card h-100">
 						<div className="card-body">
 							<h2 className="card-title">Password Generation</h2>
-							<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+							<div className="row justify-content-center">
+								<div className="col-md-10 input-group mb-3">
+									<input type="text" name="txtpassword" id="txtpassword" className="form-control" placeholder="Password" defaultValue={this.state.txtpassword} readOnly></input>
+									<div className="input-group-append">
+										<button className="btn btn-info" type="button" onClick={this.generatePassword}>Generate Password</button>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
